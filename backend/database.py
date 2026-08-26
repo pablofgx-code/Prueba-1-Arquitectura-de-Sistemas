232673d23f3aa09cc4e06afce0c77d594a8bb137
pablofgx-code <pablo.fgx@gmail.com>
2026-08-26 01:16:44 -0400

    funciono la base :D

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,23 @@
+import os
+from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from backend.perfiles.models import Perfil
 
+load_dotenv
+
 async def iniciar_base_de_datos():
-    client = AsyncIOMotorClient("mongodb://localhost:27017")
+
+    url_base_datos = os.getenv("MONGODB_URl")
+    nombre_base_datos = os.getenv("DATABASE_NAME")
+
+    if not url_base_datos or not nombre_base_datos:
+        raise ValueError("Faltan variables de entorno para la Base de Datos")
     
-    db = client.iglesia_donaciones
+    client = AsyncIOMotorClient(url_base_datos)
+    
+    db = client[nombre_base_datos]
 
     await init_beanie(database=db, document_models=[Perfil])
     
-    print("Base de datos MongoDB conectada y modelos registrados.")
+    print(f"Base de datos MongoDB conectada. {nombre_base_datos}")
---
+++ backend/perfiles/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class PerfilCreate(BaseModel):
+    nombre: str = Field(..., min_length=2, max_length=50, description="Nombre del beneficiario")
+    apellido: str = Field(..., min_length=2, max_length=50)
+    rut: str = Field(..., min_length=8, max_length=12, description="RUT con guion")
+    contacto: Optional[str] = Field(None, max_length=20)
+    fecha_nacimiento: str
+    edad: int = Field(..., ge=0, le=120, description= "la edad debe ser un numero real")
+    situacion_calle: bool = False
+    motivo_situacion: Optional[str] = None
+
+class PerfilResponse(PerfilCreate):
+    id: str
+
+
+
+
